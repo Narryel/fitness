c46a7e6814168463284f33e39c4e1fcf2e52471c
Valentin Afanasiev <narryel@gmail.com>
2020-05-04 17:34:13 +0300

    remove test and pom change to meet heroku requirements

--- src/test/java/com/narryel/fitness/FitnessApplicationTests.java
+++ src/test/java/com/narryel/fitness/FitnessApplicationTests.java
@@ -1,13 +1,8 @@
 package com.narryel.fitness;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FitnessApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
