96e03a072b008b6cd1951cd80139ab9d62f9fe3a
Valentin Afanasiev <narryel@gmail.com>
2020-05-04 23:26:24 +0300

    healthCheck log add

--- src/main/java/com/narryel/fitness/controller/HealthCheck.java
+++ src/main/java/com/narryel/fitness/controller/HealthCheck.java
@@ -1,5 +1,6 @@
 package com.narryel.fitness.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health")
+@Slf4j
 public class HealthCheck {
 
     @GetMapping
     public String checkAvailability(){
+        log.info("healthCheck all is good");
         return "Bot is feeling okay, beep beep bop";
     }
 }
